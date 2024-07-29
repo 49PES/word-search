643164a2dc8c418c65d513a74193e7588298a698
Vansh Saboo <vanshchessdude@gmail.com>
2024-07-29 15:36:41 -0400

    Adding colors

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,40 @@
+use colored::Colorize;
+use std::collections::HashSet;
 use std::fmt::Display;
 use std::fs;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+struct Posn {
+    row: usize,
+    col: usize,
+}
 
 /// Board with 2D character vector for the grid and 1D String vector for the words
 #[derive(Debug)]
 struct Board {
     grid: Vec<Vec<char>>,
     words: Vec<String>,
+    word_indices: HashSet<Posn>,
 }
 
 impl Display for Board {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        for row in &self.grid {
-            for ch in row {
-                write!(f, "{}", ch)?;
+        // for row in &self.grid {
+        //     for ch in row {
+        //         write!(f, "{}", ch)?;
+        //     }
+        //     writeln!(f)?;
+        // }
+
+        for i in 0..self.grid.len() {
+            for j in 0..self.grid[0].len() {
+                let ch: char = self.grid[i][j];
+                let posn = Posn { row: i, col: j };
+                if self.word_indices.contains(&posn) {
+                    write!(f, "{}", ch.to_string().red())?;
+                } else {
+                    write!(f, "{}", ch)?;
+                }
             }
             writeln!(f)?;
         }
@@ -21,6 +43,18 @@ impl Display for Board {
         writeln!(f, "{:?}", self.words)?;
 
         Ok(())
+    }
+}
+
+impl Board {
+    fn new(grid: Vec<Vec<char>>, words: Vec<String>) -> Self {
+        let mut indices: HashSet<Posn> = HashSet::new();
+        indices.insert(Posn { row: 1, col: 2 });
+        Board {
+            grid,
+            words,
+            word_indices: indices,
+        }
     }
 }
 
@@ -36,6 +70,6 @@ fn main() {
 
     let words: Vec<String> = words_content.lines().map(|line| line.to_string()).collect();
 
-    let board: Board = Board { grid, words };
+    let board: Board = Board::new(grid, words);
     println!("{}", board);
 }
